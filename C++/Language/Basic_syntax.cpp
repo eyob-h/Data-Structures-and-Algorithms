d167500bc19802789fdc339da5074fb673a694d3
Eyob <eyobhg3@gmail.com>
2024-01-09 13:59:11 +0300

    Add initial folder structure and basic code

---
+++ C++/Language/Basic_syntax.cpp
@@ -0,0 +1,11 @@
+// First Basic Program
+#include <iostream> //preprocessor directive
+using namespace std;// declaration for usiing all the elements in the std namespace in an unqualified manner 
+
+int main(){ //where the execution begins
+    std::cout<<"First Program!";
+}
+
+/*
+block comment
+*/ 
